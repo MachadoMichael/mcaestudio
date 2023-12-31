ca503d3ab258c55af25f2ca03e0309c96da0c5bf
Michael <machado.m3@icloud.com>
2023-12-31 10:25:34 -0300

    add: PhotoSender

--- src/views/admin/Admin.js
+++ src/views/admin/Admin.js
@@ -1,10 +1,15 @@
+import { useState } from 'react'
 import { LoginForm } from '../../components/login-form/LoginForm'
+import { PhotoSender } from '../../components/photo-sender/PhotoSender'
 import './Admin.css'
 
 export const Admin = () => {
+  const [userLogged, setUserLogged] = useState(true)
+  const [state, setState] = useState('sender')
   return (
     <div className="admin-container">
-      <LoginForm />
+      {!userLogged && <LoginForm />}
+      {userLogged && state === 'sender' && <PhotoSender />}
     </div>
   )
 }
