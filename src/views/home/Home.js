bc87de13246fa02ca76e3e61363f2273f8eb4ee3
Michael <machado.m3@icloud.com>
2024-01-16 08:03:48 -0300

    change photos to .webp

--- src/views/home/Home.js
+++ src/views/home/Home.js
@@ -3,7 +3,6 @@ import { Navbar } from '../../components/navbar';
 import './Home.css'
 import { Dictionaries } from '../../data/Dictionaries';
 
-
 export const Home = () => {
   return (
     <div className="Home">
