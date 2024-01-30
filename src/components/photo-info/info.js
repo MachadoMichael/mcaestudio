6da68d449305148350a1eec92f5cf08fd01658e4
Michael <machado.m3@icloud.com>
2024-01-29 22:47:21 -0300

    autoplay 4500

--- src/components/photo-info/info.js
+++ src/components/photo-info/info.js
@@ -2,7 +2,6 @@ import './info.css'
 
 export const Info = ({ architect, localization, companyPlace }) => {
 
-  console.log(architect, localization, companyPlace)
   return <div className="info-container">
     <div className="first-info">{architect}, {companyPlace}</div>
     <div className="second-info">{localization}</div>
--- src/components/swiper/index.js
+++ src/components/swiper/index.js
@@ -9,32 +9,8 @@ import { Info } from "../photo-info/info";
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import "swiper/css/effect-fade";
 import "react-lazy-load-image-component/src/effects/blur.css";
-import { useEffect, useState } from "react";
-
-
 
 const SwiperCarousel = ({ photos }) => {
-  const [isLoaded, setIsLoaded] = useState(false);
-
-  const loadImage = (src) => {
-    return new Promise((resolve, reject) => {
-      const img = new Image();
-      img.src = src;
-      img.onload = resolve;
-      img.onerror = reject;
-    });
-  };
-
-  useEffect(() => {
-    Promise.all(photos.map((photo) => loadImage(photo.img))).then(() =>
-      setIsLoaded(true)
-    );
-  }, [photos]);
-
-  if (!isLoaded) {
-    return <h2 style={{color:"#fff"}}>Carregando...</h2>;
-  }
-
   return (
     <div className="container-swiper">
       <Swiper
@@ -44,19 +20,14 @@ const SwiperCarousel = ({ photos }) => {
         spaceBetween={50}
         fadeEffect={{ crossFade: true }}
         slidesPerView={1}
-        onSlideChange={() => console.log("slide change")}
         onSwiper={(swiper) => {
           console.log(swiper);
         }}
-        autoplay={{
-          delay: 4000,
-          pauseOnMouseEnter: true,
-          waitForTransition: true,
-        }}
+        autoplay={{ delay: 4500, pauseOnMouseEnter: true, LazyLoadImage: true }}
       >
-        {photos.map((photo) => (
+        {photos.map((photo, i) => (
           <div>
-            <SwiperSlide>
+            <SwiperSlide key={i}>
               <div className="img-box">
                 <LazyLoadImage
                   id="photo-img"
