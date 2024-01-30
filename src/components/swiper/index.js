7fc2b59cc128ce3976eab08045c7ef7f8d52d421
Michael <machado.m3@icloud.com>
2024-01-29 22:09:43 -0300

    pre-load photos

--- src/components/swiper/index.js
+++ src/components/swiper/index.js
@@ -9,16 +9,38 @@ import { Info } from "../photo-info/info";
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import "swiper/css/effect-fade";
 import "react-lazy-load-image-component/src/effects/blur.css";
+import { useEffect, useState } from "react";
+
+const loadImage = (src) => {
+  return new Promise((resolve, reject) => {
+    const img = new Image();
+    img.src = src;
+    img.onload = resolve;
+    img.onerror = reject;
+  });
+};
 
 const SwiperCarousel = ({ photos }) => {
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    Promise.all(photos.map((photo) => loadImage(photo.img))).then(() =>
+      setIsLoaded(true)
+    );
+  }, [photos]);
+
+  if (!isLoaded) {
+    return <h2 style={{color:"#fff"}}>Carregando...</h2>;
+  }
+
   return (
     <div className="container-swiper">
       <Swiper
         modules={[Autoplay, EffectFade]}
         pagination={{ clickable: true }}
         effect="fade"
-        fadeEffect={{ crossFade: true }}
         spaceBetween={50}
+        fadeEffect={{ crossFade: true }}
         slidesPerView={1}
         onSlideChange={() => console.log("slide change")}
         onSwiper={(swiper) => {
@@ -29,7 +51,6 @@ const SwiperCarousel = ({ photos }) => {
           pauseOnMouseEnter: true,
           waitForTransition: true,
         }}
-        speed={1300}
       >
         {photos.map((photo) => (
           <div>
