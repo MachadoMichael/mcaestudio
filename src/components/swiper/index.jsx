85d258b5a2b56c8e4ada55c2591046383bb70a7b
Michael <machado.m3@icloud.com>
2024-02-02 08:18:29 -0300

    fix: remove blur in componet to resolve the crash error in mobile

--- src/components/swiper/index.jsx
+++ src/components/swiper/index.jsx
@@ -8,7 +8,6 @@ import { Autoplay, EffectFade } from "swiper/modules";
 import { Info } from "../photo-info/info";
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import "swiper/css/effect-fade";
-import "react-lazy-load-image-component/src/effects/blur.css";
 
 const SwiperCarousel = ({ photos }) => {
   return (
@@ -20,7 +19,12 @@ const SwiperCarousel = ({ photos }) => {
         spaceBetween={50}
         fadeEffect={{ crossFade: true }}
         slidesPerView={1}
-        autoplay={{ delay: 4500, pauseOnMouseEnter: true, LazyLoadImage: true }}
+        autoplay={{
+          delay: 4500,
+          pauseOnMouseEnter: true,
+          LazyLoadImage: true,
+          stopOnLastSlide: true,
+        }}
       >
         {photos.map((photo, i) => (
           <SwiperSlide key={i}>
@@ -29,7 +33,6 @@ const SwiperCarousel = ({ photos }) => {
                 id="photo-img"
                 src={photo.img}
                 alt={photo.alt}
-                effect="blur"
               />
               <Info
                 architect={photo.architect}
--- src/views/home/Home.js
+++ src/views/home/Home.js
@@ -1,89 +1,37 @@
 import { Navbar } from '../../components/navbar';
 import './Home.css'
 import { Dictionaries } from '../../data/Dictionaries';
-import { useState } from 'react';
-import { LazyLoadImage } from 'react-lazy-load-image-component';
+import { useEffect, useState } from 'react';
 
-import 'react-lazy-load-image-component/src/effects/blur.css';
-import { Info } from '../../components/photo-info/info';
-import PlayArrowIcon from '@mui/icons-material/PlayArrow';
-import PauseIcon from '@mui/icons-material/Pause';
-import NavigateNextIcon from '@mui/icons-material/NavigateNext';
-import NavigateBeforeIcon from '@mui/icons-material/NavigateBefore';
-import SkipNextIcon from '@mui/icons-material/SkipNext';
-import SkipPreviousIcon from '@mui/icons-material/SkipPrevious';
-import { motion } from 'framer-motion'
+import 'react-lazy-load-image-component/src/effects/blur.css'
 import SwiperCarousel from '../../components/swiper';
 
 export const Home = () => {
-  const [pause, setPause] = useState(true)
-  const [selectedPhoto, setSelectedPhoto] = useState(0);
-  let timer;
+  const [cleanCookies, setCleanCookies] = useState(false)
 
-  const changePhoto = (side) => {
-    if (side === 'next' && selectedPhoto < Dictionaries.length - 1) setSelectedPhoto(selectedPhoto + 1)
-    if (side === 'prev' && selectedPhoto > 0) setSelectedPhoto(selectedPhoto - 1)
-  }
-
-  function handleInterval() {
-    if (pause && selectedPhoto < Dictionaries.length - 1) {
-      setPause(false)
-      setTimeout(() => setSelectedPhoto(selectedPhoto => selectedPhoto + 1))
+  useEffect(() => {
+    if (!document.__defineGetter__) {
+      Object.defineProperty(document, 'cookie', {
+        get: function() { return '' },
+        set: function() { return true },
+      });
     } else {
-      setPause(true)
-      clearTimeout(timer)
+      document.__defineGetter__("cookie", function() { return ''; });
+      document.__defineSetter__("cookie", function() { });
     }
-  }
 
-  function smoothTransition() {
-    if (!pause && selectedPhoto < Dictionaries.length - 1) timer = setTimeout(() => setSelectedPhoto(selectedPhoto => selectedPhoto + 1), 4000)
-    if (pause) clearTimeout(timer)
-  }
-  //
-  // return (
-  //   <div className="Home">
-  //     <Navbar />
-  //     <div className='home-body'>
-  //       <div id='square'>
-  //         <div id='lazy-image-container'>
-  //
-  //           <motion.div
-  //             key={selectedPhoto}
-  //             animate={{ opacity: [0, 1] }}
-  //             transition={{ duration: 1, times: [0, 1], loop: true }}
-  //           >
-  //             <LazyLoadImage
-  //               id={`lazy-image`}
-  //               className={`image`}
-  //               src={Dictionaries[selectedPhoto].img}
-  //               effect='blur'
-  //               onLoad={smoothTransition}
-  //             />
-  //           </motion.div>
-  //         </div>
-  //         <Info architect={Dictionaries[selectedPhoto]["architect"]} companyPlace={Dictionaries[selectedPhoto]["companyPlace"]} localization={Dictionaries[selectedPhoto]["localization"]} />
-  //         <div id='controllers'>
-  //           <SkipPreviousIcon className='button' onClick={() => setSelectedPhoto(0)} sx={{ color: 'whitesmoke' }} />
-  //           <NavigateBeforeIcon className='button' onClick={() => changePhoto('prev')} sx={{ color: selectedPhoto === 0 ? 'disable' : 'whitesmoke' }} />
-  //           {pause
-  //             ? <PlayArrowIcon className='button' onClick={handleInterval} sx={{ color: 'whitesmoke' }} />
-  //             : <PauseIcon className='button' onClick={handleInterval} sx={{ color: 'whitesmoke' }} />
-  //           }
-  //           <NavigateNextIcon className='button' onClick={() => changePhoto('next')} sx={{ color: selectedPhoto === 63 ? 'disable' : 'whitesmoke' }} />
-  //           <SkipNextIcon className='button' onClick={() => setSelectedPhoto(63)} sx={{ color: 'whitesmoke' }} />
-  //
-  //         </div>
-  //       </div>
-  //     </div>
-  //   </div>
-  // );
-  //
-  return (
-    <div className='Home'>
-      <Navbar />
-      <div className='home-body'>
-        <SwiperCarousel photos={Dictionaries} />
+    setCleanCookies(true)
+  }, [])
+  if (cleanCookies) {
+    return (
+      <div className='Home'>
+        <Navbar />
+        <div className='home-body'>
+          <SwiperCarousel photos={Dictionaries} />
+        </div>
       </div>
-    </div>
-  )
+    )
+
+
+  }
 }
